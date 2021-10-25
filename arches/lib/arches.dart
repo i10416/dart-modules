c3d42b88eeaf12d56c322948b1cbb05f433311df
i10416 <ito.yo16uh90616@gmail.com>
2021-10-25 18:54:48 +0900

    add stream listener

--- arches/lib/arches.dart
+++ arches/lib/arches.dart
@@ -1,2 +1,4 @@
 export 'components/collection/inkwell_list_tile.dart';
 export 'components/icon/gradient_icon.dart';
+export 'components/utility/async/async_command_widget.dart';
+export 'components/utility/async/stream_listener.dart';
---
+++ arches/lib/components/utility/async/stream_listener.dart
@@ -0,0 +1,36 @@
+import 'dart:async';
+
+import 'package:flutter/widgets.dart';
+
+class StreamListener<T> extends StatefulWidget {
+  const StreamListener(
+      {required this.stream, required this.builder, required this.listener});
+
+  final Stream<T> stream;
+  final Widget Function(BuildContext) builder;
+  final void Function(T, [BuildContext?]) listener;
+
+  @override
+  _StreamListenerState<T> createState() => _StreamListenerState();
+}
+
+class _StreamListenerState<T> extends State<StreamListener<T>> {
+  late final StreamSubscription<T> _subscription;
+
+  @override
+  void initState() {
+    super.initState();
+    _subscription = widget.stream.listen((event) {
+      widget.listener(event, context);
+    });
+  }
+
+  @override
+  void dispose() {
+    _subscription.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) => widget.builder(context);
+}
