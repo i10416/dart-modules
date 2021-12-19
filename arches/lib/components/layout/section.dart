45d8277d6bdebb532de61829f123664b06a750e1
i10416 <ito.yo16uh90616@gmail.com>
2021-12-19 18:28:01 +0900

    small update

--- arches/lib/components/layout/section.dart
+++ arches/lib/components/layout/section.dart
@@ -9,27 +9,24 @@ class Section extends StatelessWidget {
   final Widget? subtitle;
 
   @override
-  Widget build(BuildContext context) => SizedBox(
-        width: MediaQuery.of(context).size.width,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            if (title != null)
-              Padding(
-                  padding: const EdgeInsets.fromLTRB(12, 36, 12, 0),
-                  child: title),
-            if (subtitle != null)
-              Padding(
-                  padding: const EdgeInsets.fromLTRB(12, 4, 12, 0),
-                  child: subtitle),
-            const Padding(padding: EdgeInsets.only(bottom: 12)),
-            Container(
-              decoration: BoxDecoration(
-                color: Theme.of(context).canvasColor,
-              ),
-              child: child,
-            )
-          ],
-        ),
+  Widget build(BuildContext context) => Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          if (title != null)
+            Padding(
+                padding: const EdgeInsets.fromLTRB(12, 36, 12, 0),
+                child: title),
+          if (subtitle != null)
+            Padding(
+                padding: const EdgeInsets.fromLTRB(12, 4, 12, 0),
+                child: subtitle),
+          const SizedBox(height: 12),
+          Container(
+            decoration: BoxDecoration(
+              color: Theme.of(context).canvasColor,
+            ),
+            child: child,
+          )
+        ],
       );
 }
